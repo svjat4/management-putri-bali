5975845a2bbfcf95f440471119ea4835ed04dcae
lazyfay <rifaiarsaa@gmail.com>
2026-06-16 21:20:57 +0700

    fix: remove unused framer-motion

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
-import { bgBlur } from "framer-motion/client";
-import { mix } from "framer-motion";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs);
-}
-
-export function slugify(text: string): string {
-  return text
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, '-')
-    .replace(/[^\w\-]+/g, '')
-    .replace(/\-\-+/g, '-')
-    .replace(/^-+/, '')
-    .replace(/-+$/, '');
+  return twMerge(clsx(inputs));
 }
